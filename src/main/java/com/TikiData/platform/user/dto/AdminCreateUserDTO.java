0c84603ca7e40c102b8596d2a528f41a3c4c2b00
sltobias7 <sltobias7@gmail.com>
2026-06-02 16:09:55 -0300

    Creacion de DTO para registra administrador

---
+++ src/main/java/com/TikiData/platform/user/dto/AdminCreateUserDTO.java
@@ -0,0 +1,28 @@
+package com.TikiData.platform.user.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AdminCreateUserDTO {
+    @NotBlank(message = "El email es obligatorio")
+    @Email(message = "Debe ser un formato de email válido")
+    private String email;
+
+    @NotBlank(message = "La contraseña es obligatoria")
+    @Size(min = 6, message = "La contraseña debe tener al menos 6 caracteres")
+    private String password;
+
+    @NotBlank(message = "El rol es obligatorio")
+    @Pattern(regexp = "^(USER|ADMIN)$", message = "El rol debe ser USER o ADMIN")
+    private String role;
+}
