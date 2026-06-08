1bea58459c202e6b80733acb1fde336cfaf0bfde
juandesiena <juandesiena@gmail.com>
2026-06-08 14:53:49 -0300

    Championship controller finalizado

--- src/main/java/com/TikiData/platform/Championship/Controller/ChampionshipController.java
+++ src/main/java/com/TikiData/platform/Championship/Controller/ChampionshipController.java
@@ -1,13 +1,50 @@
 package com.TikiData.platform.Championship.Controller;
 
+import com.TikiData.platform.Championship.DTO.ChampionshipRequestDTO;
+import com.TikiData.platform.Championship.DTO.ChampionshipResponseDTO;
+import com.TikiData.platform.Championship.Mapper.ChampionshipMapper;
+import com.TikiData.platform.Championship.Model.Championship;
 import com.TikiData.platform.Championship.Service.ChampionshipService;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/championship")
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class ChampionshipController {
+
+    private final ChampionshipService championshipService;
+
+    @GetMapping
+    public ResponseEntity<List<ChampionshipResponseDTO>> getAllChampionships() {
+        return ResponseEntity.ok(championshipService.findAllChampionships());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ChampionshipResponseDTO> getChampionshipById(@PathVariable String nombre) {
+        return ResponseEntity.ok(championshipService.findByName(nombre));
+    }
+
+    @PostMapping
+    public ResponseEntity<ChampionshipResponseDTO> saveChampionship(@RequestBody @Valid ChampionshipRequestDTO championshipRequestDTO) {
+        return ResponseEntity.ok(championshipService.saveChampionship(championshipRequestDTO));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ChampionshipResponseDTO> updateChampionship(@PathVariable Long id, @RequestBody @Valid ChampionshipRequestDTO championshipRequestDTO) {
+        return ResponseEntity.ok(championshipService.updateChampionship(championshipRequestDTO, id));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteChampionship(@PathVariable Long id) {
+        championshipService.deleteChampionshipById(id);
+        return ResponseEntity.noContent().build();
+    }
+
 
 }
---
+++ src/main/java/com/TikiData/platform/Common/Exception/ChampionshipNotFoundException.java
@@ -0,0 +1,7 @@
+package com.TikiData.platform.Common.Exception;
+
+public class ChampionshipNotFoundException extends RuntimeException {
+    public ChampionshipNotFoundException(String message) {
+        super(message);
+    }
+}
