642d668d82e02db2f80ce17bfc701e5db0f7f53e
juandesiena <juandesiena@gmail.com>
2026-06-14 23:36:32 -0300

    Avance en Global Handler Exception

---
+++ src/main/java/com/TikiData/platform/Common/Exception/ErrorMessage.java
@@ -0,0 +1,14 @@
+package com.TikiData.platform.Common.Exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ErrorMessage {
+    private String error;
+}
--- src/main/java/com/TikiData/platform/Common/Exception/GlobalExceptionHandler.java
+++ src/main/java/com/TikiData/platform/Common/Exception/GlobalExceptionHandler.java
@@ -1,4 +1,54 @@
 package com.TikiData.platform.Common.Exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(NewsNotFoundException.class)
+    public ResponseEntity<ErrorMessage> handlerForNewsNotFoundException(NewsNotFoundException ex) {
+        ErrorMessage error = new ErrorMessage(ex.getMessage());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+    }
+
+    @ExceptionHandler(PlayerNotFoundException.class)
+    public ResponseEntity<ErrorMessage> handlerForPlayerNotFoundException(PlayerNotFoundException ex) {
+        ErrorMessage error = new ErrorMessage(ex.getMessage());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+    }
+
+    @ExceptionHandler(TeamNotFoundException.class)
+    public ResponseEntity<ErrorMessage> handlerForTeamNotFoundException(TeamNotFoundException ex) {
+        ErrorMessage error = new ErrorMessage(ex.getMessage());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+    }
+
+    @ExceptionHandler(ResourceAlreadyExistsException.class)
+    public ResponseEntity<ErrorMessage> handlerForResourceAlreadyExistsException(ResourceAlreadyExistsException ex) {
+        ErrorMessage error = new ErrorMessage(ex.getMessage());
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(error);
+    }
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ErrorMessage> handlerForResourceNotFoundException(ResourceNotFoundException ex) {
+        ErrorMessage error = new ErrorMessage(ex.getMessage());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handlerForMethodInvalidException(MethodArgumentNotValidException ex) {
+        Map<String, String> mapa = new HashMap<>();
+        ex.getBindingResult().getFieldErrors().forEach(
+                error -> mapa.put(error.getField(), error.getDefaultMessage()));
+
+        return ResponseEntity.status(HttpStatusCode.valueOf(400)).body(mapa);
+    }
 }
